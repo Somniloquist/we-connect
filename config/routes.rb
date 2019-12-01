9be3952ecae16dc6ecb2d1802e4b540c80c4862d
Robin Unger <robin.e.unger@gmail.com>
2019-12-01 14:10:27 -0600

    setup devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   devise_for :users
+
+  as :user do
+    get     "signup", to: "devise/registrations#new"
+    post    "signup", to: "devise/registrations#create"
+    get     "signin", to: "devise/sessions#new"
+    post    "signin", to: "devise/sessions#create"
+    delete  "signout", to: "devise/sessions#destroy"
+  end
 end
