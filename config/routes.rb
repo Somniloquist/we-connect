e2b4947ec98f4077ed87f38a8c8900475a536549
Robin Unger <robin.e.unger@gmail.com>
2019-12-07 14:39:46 -0600

    Add friendships controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   devise_for :users
-  resources :users, only: [:index, :show]
-
-  # as :user do
-  #   get     "signup", to: "devise/registrations#new"
-  #   post    "signup", to: "devise/registrations#create"
-  #   get     "signin", to: "devise/sessions#new"
-  #   post    "signin", to: "devise/sessions#create"
-  #   delete  "signout", to: "devise/sessions#destroy"
-  # end
+  resources :users, only: [:index, :show] do
+    resources :friendships, only: [:index, :new, :create, :destroy]
+  end
 end
