73d36a16f3ca1922a3672e2d813800791649279e
Robin Unger <robin.e.unger@gmail.com>
2019-12-14 20:41:40 -0600

    Add accepted? column to friendhip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_012528) do
+ActiveRecord::Schema.define(version: 2019_12_15_015750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,19 +20,20 @@ ActiveRecord::Schema.define(version: 2019_12_07_012528) do
     t.bigint "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "accepted?", default: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
-  create_table "relationships", force: :cascade do |t|
+  create_table "ralationships", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["friend_id"], name: "index_relationships_on_friend_id"
-    t.index ["user_id", "friend_id"], name: "index_relationships_on_user_id_and_friend_id", unique: true
-    t.index ["user_id"], name: "index_relationships_on_user_id"
+    t.index ["friend_id"], name: "index_ralationships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_ralationships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_ralationships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
