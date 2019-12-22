23b71cf60fa9c07e581b22061c76cc7186ed6ff9
Robin Unger <robin.e.unger@gmail.com>
2019-12-21 18:41:25 -0600

    Create post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_220421) do
+ActiveRecord::Schema.define(version: 2019_12_22_003206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2019_12_18_220421) do
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +53,5 @@ ActiveRecord::Schema.define(version: 2019_12_18_220421) do
   end
 
   add_foreign_key "friendships", "users"
+  add_foreign_key "posts", "users"
 end
