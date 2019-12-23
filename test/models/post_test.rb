4373ef085d7a60eafa9b5d53d14ad3cd3c6d7b34
Robin Unger <robin.e.unger@gmail.com>
2019-12-22 22:13:30 -0600

    Post model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users :john
+    @post = @user.posts.build(body: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "body text should be present" do
+    @post.body = "   "
+    assert_not @post.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal posts(:most_recent), Post.first
+  end
 end
