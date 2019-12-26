a0dcd2635ead411cf0bb43acfeaabdde172bd054
Robin Unger <robin.e.unger@gmail.com>
2019-12-26 11:34:09 -0600

    Display posts on user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
     @number_of_friends = Friendship.joins(:user).where(user_id: @user, accepted?: true).count
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   root "static_pages#home"
   devise_for :users
   resources :friendships, only: [:create, :destroy, :update]
+  resources :posts
   resources :users, only: [:index, :show] do
     member do
       get :friends
+      get :posts
     end
   end
 end
