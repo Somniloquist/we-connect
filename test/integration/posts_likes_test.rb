180714273c3ff20e1e27ecab4dd82e15e8527c35
Robin Unger <robin.e.unger@gmail.com>
2019-12-29 17:35:28 -0600

    Write for post like counts

---
+++ test/integration/posts_likes_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class LikesTest < ActionDispatch::IntegrationTest
+  include ActionView::Helpers::TextHelper
+
+  def setup
+    @user = users(:john)
+    @post = posts(:apple)
+  end
+
+  test "post likes page shows correctly" do
+    get likes_post_path(@post)
+    assert_not @post.likes.empty?
+    assert_not @post.likes_users.empty?
+    @post.likes_users.each do |user|
+      assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "liking a post updates the like count" do 
+    get post_path(@post)
+    likes = @post.likes.count
+    assert_match "#{pluralize(likes, 'like')}", response.body
+    post likes_path, params: { post_id: @post.id, user_id: @user.id }
+    follow_redirect!
+    assert_match "#{pluralize(likes + 1, 'like')}", response.body
+  end
+end
