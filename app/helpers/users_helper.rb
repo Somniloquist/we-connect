66a9544225b137f608a85531de744575530cbd9c
Robin Unger <robin.e.unger@gmail.com>
2020-01-16 13:49:13 -0600

    Update avatar style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,12 +2,12 @@ module UsersHelper
   def gravatar_for(user, size=80)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: "#{user.firstname} #{user.lastname}", class: "gravatar")
+    image_tag(gravatar_url, alt: "#{user.firstname} #{user.lastname}", class: "avatar gravatar")
   end
 
   # Set custom avatar if user has uploaded one else use gravatar
   def avatar_for(user, size=80)
-    user.avatar.attached? ? image_tag(url_for(user.set_avatar(size))) : gravatar_for(user, size)
+    user.avatar.attached? ? image_tag(url_for(user.set_avatar(size)), class: "avatar") : gravatar_for(user, size)
   end
 
   # creates or loads a variant of height, use to generate 
