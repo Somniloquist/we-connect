945ba070575d7009407eb05c8f8ebd1a9d2a70bb
Robin Unger <robin.e.unger@gmail.com>
2020-01-24 13:51:18 -0600

    Paginate posts in user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @banner_
     @date_joined = @user.created_at.strftime("%B %Y")
-    @posts = @user.posts
+    @posts = @user.posts.paginate(page: params[:page], per_page: 20)
     @number_of_friends = Friendship.joins(:user).where(user_id: @user, accepted?: true).count
   end
 
