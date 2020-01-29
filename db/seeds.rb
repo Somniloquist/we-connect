ede11fd69775e24343f4e8e62f05533bfc1c1243
Robin Unger <robin.e.unger@gmail.com>
2020-01-29 15:49:04 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(firstname: "Test",
-            lastname: "User",
-            email: "test@example.com",
-            password: "password",
-            birthday: Time.now)
-
 99.times do |i|
   User.create!(firstname: Faker::Name.first_name,
               lastname: Faker::Name.last_name,
-              email: "example#{i}@example.com",
+              email: "test#{i+1}@example.com",
               password: "password",
-              gender: Faker::Gender.binary_type)
+              gender: Faker::Gender.binary_type,
+              about: Faker::Quote.famous_last_words)
 end
 
 User.create!(firstname: "Robin",
             lastname: "Unger",
             email: "robin.e.unger@gmail.com",
             password: "password",
-            birthday: Time.now)
+            birthday: Time.now,
+            about: "Developer")
 
-
-
+User.update_all confirmed_at: DateTime.now
 
 users = User.order(:created_at).take(10)
 50.times do
-  content = Faker::Lorem.paragraph
-  users.each { |user| user.posts.create!(body: content) }
+  users.each { |user| user.posts.create!(body: Faker::Hipster.paragraph) }
 end
 
 users = User.all
 user = users.last
 
-users[1..10].each { |friend| user.add_friend(friend) }
+users[0...10].each { |friend| user.add_friend(friend) }
+
+users[0...10].each do |user|
+  file = open("https://robohash.org/#{user.lastname}")
+  user.avatar.attach(
+    io: file,
+    filename: "avatar.png",
+    content_type: file.content_type
+  )
+end
+
 Friendship.all.each { |friendship| friendship.accept }
