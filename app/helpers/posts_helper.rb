822f237ef70f55e7a519270f9f6f7477b84b8dfe
Robin Unger <robin.e.unger@gmail.com>
2020-02-08 12:36:51 -0600

    Update post style

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
 
-  def image_for(post, size=500, crop=false)
-    image_tag(url_for(post.set_image(size, crop)), class: "post-image") 
+  def image_for(post, size=500, crop=false, css_class="image-large")
+    image_tag(url_for(post.set_image(size, crop)), class: css_class) 
   end
 
 end
